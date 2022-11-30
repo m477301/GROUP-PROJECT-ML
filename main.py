17e5d5aa6a8f993512b1af555abb4a40e2c0f48b
Vitali Borsak <borsak.vitali@gmail.com>
2022-11-30 13:42:40 +0000

    Built on the webscraper and added multiple books to be scraped at once

--- main.py
+++ main.py
@@ -1,33 +1,99 @@
 from bs4 import BeautifulSoup
 import requests 
 import json
+import re
 
-books = []
+data = open('data.json')
+books = json.load(data)
+data.close()
 
-url = "https://www.goodreads.com/book/show/5100"
+for i in range(1, 10):
+    url = f"https://www.goodreads.com/book/show/{i}"
 
-result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "html.parser")
 
-with open('html.txt', 'w', encoding="utf-8") as f:
-    f.write(str(doc.prettify()))
+    with open('html.txt', 'w', encoding="utf-8") as f:
+        f.write(str(doc.prettify()))
 
-# Type of Html Received
-html = doc.find(class_= "desktop withSiteHeaderTopFullImage")
-title = "N/A"
+    # Type of Html Received
+    html = doc.find(class_= "desktop withSiteHeaderTopFullImage")
+    title = "N/A"
 
-if html:
-    h1Title = doc.find(id="bookTitle")
-    title = str(h1Title.text.strip())
-else: 
-    h1Title = doc.find(class_="Text Text__title1")
-    title = str(h1Title.text.strip())
+    if html:
+        # Get the title
+        h1Title = doc.find(id="bookTitle")
+        title = str(h1Title.text.strip())
 
-book = {
-    "title": title
-}
+        # Get the blurb
+        blurbClass = doc.find(class_="readable stacked")
+        blurb = str(blurbClass.text.strip())
+        blurb = blurb.translate({ord(i): None for i in '"'})
 
-books.append(book)
+        # Get the genres
+        actionLinkGenres = doc.find_all(class_="actionLinkLite bookPageGenreLink") 
+        genres = []
+        for genre in actionLinkGenres:
+            genres.append(str(genre.text.strip()))
+        
+        # Get author
+        authorName = doc.find(class_="authorName")
+        author = str(authorName.text.strip())
+
+        # Get book rating
+        bookRating = doc.find("span", itemprop="ratingValue")
+        rating = str(bookRating.text.strip())
+
+        # Get pages & format
+        PageCount = doc.find("span", itemprop='numberOfPages')
+        if PageCount:
+            pages = str(PageCount.contents[0].text.strip()).split(" ")[0]
+        else:
+            pages = None    
+
+        # Get publishing date
+        PublishingDate = doc.find(class_="row")
+        date = str(PublishingDate.contents[1].text.strip())
+    else: 
+        # Get the title
+        h1Title = doc.find(class_="Text Text__title1")
+        title = str(h1Title.text.strip())
+
+        # Get the blurb
+        blurbClass = doc.find(class_="Formatted")
+        blurb = str(blurbClass.text.strip())
+        blurb = blurb.translate({ord(i): None for i in '"'})
+        
+        # Get the genres of the book
+        actionLinkGenres = doc.find_all(class_="Button Button--tag-inline Button--small") 
+        genres = []
+        for genre in actionLinkGenres:
+            genres.append(str(genre.text.strip()))
+        
+        # Get author
+        authorName = doc.find(class_="ContributorLink__name")
+        author = str(authorName.text.strip())
+
+        # Get book rating
+        bookRating = doc.find(class_="RatingStatistics__rating")
+        rating = str(bookRating.text.strip())
+
+        # Get pages & format and publishing date
+        extraInfo = doc.find(class_='FeaturedDetails')
+        pages = str(extraInfo.contents[0].text.strip()).split(" ")[0]
+        date = str(extraInfo.contents[1].text.strip()).split(" ")[-1]
+
+    book = {
+        "title": title,
+        "blurb": blurb,
+        "genre": genres,
+        "author": author,
+        "rating": rating,
+        "pages": pages,
+        "date": date
+    }
+
+    books.append(book)
 
 jsonString = json.dumps(books)
 jsonFile = open("data.json", "w")
